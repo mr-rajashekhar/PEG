1e665716ed2a97eb241b980197130d604224114a
thelo09 <200030058@iitdh.ac.in>
2023-11-30 18:48:03 +0530

    Nav-bar,Home,People modified

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -3,8 +3,7 @@ showSlides();
 
 function showSlides() {
   let i;
-  const slides = document.getElementsByClassName("mySlides");
-  const dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -12,10 +11,6 @@ function showSlides() {
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 5 seconds
 }
